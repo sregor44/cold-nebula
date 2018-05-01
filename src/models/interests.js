525fd3612eac7471f23b041742b54b7d902efbcd
dgleiche <dgleiche@me.com>
2018-05-01 02:15:04 -0400

    Schema updated/should be ready

--- src/models/interests.js
+++ src/models/interests.js
@@ -8,16 +8,35 @@ async function init(db) {
 
   db.none(`
     CREATE TABLE IF NOT EXISTS users_interests(
+      interest_id INT
+            REFERENCES interests
+              ON DELETE CASCADE
+              ON UPDATE CASCADE,
+      user_id INT
+            REFERENCES users
+              ON DELETE CASCADE
+              ON UPDATE CASCADE,
+      PRIMARY KEY (user_id, interest_id)
+    );
+  `);
+
+  db.none(`
+    CREATE TABLE IF NOT EXISTS clubs(
       id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-      interest_id INT NOT NULL,
-      user_id INT NOT NULL
+      interest_id INT
+            REFERENCES interests
+              ON DELETE CASCADE
+              ON UPDATE CASCADE,
+      club_name varchar(255) NOT NULL,
+      description varchar(255) NOT NULL
     );
   `);
 }
 
 async function destroy(db) {
   db.none('DROP TABLE IF EXISTS interests;');
-  db.none('DROP TABLE IF EXSISTS users_interests');
+  db.none('DROP TABLE IF EXISTS clubs;')
+  db.none('DROP TABLE IF EXSISTS users_interests;');
 }
 
 async function insert(db, groupName) {
@@ -32,9 +51,18 @@ async function insert(db, groupName) {
 
 async function addUserInterest(db, userId, interestId) {
   db.none(`
-    INSERT INTO users_interests(user_id, interest_id)
+    INSERT INTO users_interests (user_id, interest_id)
     VALUES ($1, $2);
   `);
+}
+
+async function addClub(db, interestId, clubName, description) {
+  const stmt = `
+    INSERT INTO clubs (interest_id, club_name, description)
+    VALUES ($1, $2, $3);
+  `;
+
+  db.none(stmt, [interestId, clubName, description]);
 }
 
 module.exports = {
@@ -42,4 +70,5 @@ module.exports = {
   destroy,
   insert,
   addUserInterest,
+  addClub,
 };
